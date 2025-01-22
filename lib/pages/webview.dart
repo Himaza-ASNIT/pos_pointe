c7cbe17c1c3d6b781d94516009e405788ac8f986
himaza99 <himazamifthah@gmail.com>
2025-01-22 23:06:35 +0530

    chore: update Android Gradle plugin to version 8.2.1 and refactor WebViewScreen layout

--- lib/pages/webview.dart
+++ lib/pages/webview.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewScreen extends StatefulWidget {
@@ -21,7 +22,6 @@ class _WebViewScreenState extends State<WebViewScreen> {
     "https://mypospointe.com/modifers",
     "https://mypospointe.com/flash_report",
     "https://mypospointe.com/mobileview",
-
   ];
 
   late WebViewController _webViewController;
@@ -42,11 +42,10 @@ class _WebViewScreenState extends State<WebViewScreen> {
       return 'https://mypospointe.com/items';
     } else if (_currentIndex == 2) {
       return 'https://mypospointe.com/modifers';
-    } else if(_currentIndex == 3){
+    } else if (_currentIndex == 3) {
       return 'https://mypospointe.com/flash_report';
-    }else{
-            return 'https://mypospointe.com/mobileview';
-
+    } else {
+      return 'https://mypospointe.com/mobileview';
     }
   }
 
@@ -93,62 +92,60 @@ class _WebViewScreenState extends State<WebViewScreen> {
   Widget build(BuildContext context) {
     // ignore: deprecated_member_use
     return WillPopScope(
-      onWillPop: () async {
-        if(Navigator.of(context).canPop()){
-          Navigator.of(context).pop();
+        onWillPop: () async {
+          SystemNavigator.pop();
           return false;
-        }else{
-          return true;
-        }
-      },
-      child: Scaffold(
-      appBar: AppBar(
-        title: Text("My POS Pointe"),
-      ),
-      body: WebViewWidget(controller: _webViewController),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: _onBottomNavigationTapped, // Handle bottom navigation tap
-        type: BottomNavigationBarType.fixed,
-        iconSize: 28,
-        selectedLabelStyle: const TextStyle(
-          color: Color.fromARGB(255, 240, 239, 239),
-          fontWeight: FontWeight.w600,
-        ),
-        selectedItemColor: const Color.fromARGB(255, 150, 147, 147),
-        unselectedItemColor: const Color.fromRGBO(0, 0, 0, 1),
-        selectedFontSize: 15,
-        unselectedFontSize: 15,
-        selectedIconTheme: const IconThemeData(
-          color: const Color.fromARGB(255, 150, 147, 147),
-        ),
-        unselectedIconTheme: const IconThemeData(
-          color: Color.fromRGBO(5, 5, 5, 1),
-        ),
-        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.dashboard),
-            label: "Dashboard",
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("My POS Pointe"),
+            automaticallyImplyLeading: false,
+            backgroundColor: const Color.fromARGB(255, 255, 255, 255),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_shopping_cart_outlined),
-            label: "Items",
+          body: WebViewWidget(controller: _webViewController),
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: _currentIndex,
+            onTap: _onBottomNavigationTapped, // Handle bottom navigation tap
+            type: BottomNavigationBarType.fixed,
+            iconSize: 20,
+            selectedLabelStyle: const TextStyle(
+              color: Color.fromARGB(255, 240, 239, 239),
+              fontWeight: FontWeight.w500,
+            ),
+            selectedItemColor: const Color.fromARGB(255, 150, 147, 147),
+            unselectedItemColor: const Color.fromRGBO(0, 0, 0, 1),
+            selectedFontSize: 15,
+            unselectedFontSize: 15,
+            selectedIconTheme: const IconThemeData(
+              color: const Color.fromARGB(255, 150, 147, 147),
+            ),
+            unselectedIconTheme: const IconThemeData(
+              color: Color.fromRGBO(5, 5, 5, 1),
+            ),
+            backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.dashboard),
+                label: "Dashboard",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.add_shopping_cart_outlined),
+                label: "Items",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.fastfood_outlined),
+                label: "Modifers",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.book_outlined),
+                label: "Flash Report",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.menu_outlined),
+                label: "Menu",
+              ),
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.fastfood_outlined),
-            label: "Modifers",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.book_outlined),
-            label: "Flash Report",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.menu_outlined),
-            label: "Menu",
-          ),
-        ],
-      ),
-     ) );
+        ));
   }
 }
